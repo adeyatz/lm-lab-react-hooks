2cb1dff1e01ea9ed02b5221161df631225d582ef
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-10-24 18:19:52 +0100

    feat: impl. APICall with useEffect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
+import { Todo } from "../use_x/todo";
+
+type Todo = {
+  userId: string;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 // make an api call using useEffect
+const jsonUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
 export const APICall = () => {
+  const [myJson, setMyJson] = useState<null | Todo>(null);
+  const [error, setError] = useState<null | Error>(null);
 
-    // Your code here!
+  useEffect(() => {
+    const fetchJson = async (url: string) => {
+      const response = await fetch(url);
+      const myJson = await response.json();
+      console.log(myJson);
+      setMyJson(myJson);
+    };
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+    fetchJson(jsonUrl).catch((error) => setError(error));
+  }, []);
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <pre>{myJson !== null && JSON.stringify(myJson, null, 2)}</pre>
+    </>
+  );
+};
